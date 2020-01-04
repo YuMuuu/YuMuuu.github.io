d960cb7213ba205f82858429790368be88259fe4
三浦勇 <c01150788f@edu.teu.ac.jp>
2020-01-05 01:23:19 +0900

    add section

--- src/Main.elm
+++ src/Main.elm
@@ -172,11 +172,61 @@ footer1 = footer [class "fotter"]
 
 homePage : Html msg
 homePage =
-  p [] [ text "Hello World!" ]
+  div []
+    [ nav [class "panel"]
+      [ p [class "panel-heading"] [text "Home"]
+      , p [class "panel-block"] [ text "Hello World!" ]]
+    , nav [class "panel"]
+      [ p [class "panel-heading"] [ text "use technology"]
+      , a [class "panel-block"] [
+        span [class "panel-icon"] [
+            i [class "fas fa-book", attribute "aria-hidden" "true"] []
+            , text "elm"
+          ]
+        ]
+       , a [class "panel-block"] [
+        span [class "panel-icon"] [
+            i [class "fas fa-book", attribute "aria-hidden" "true"] []
+            , text "bulma"
+          ]
+        ]
+      , a [class "panel-block"] [
+        span [class "panel-icon"] [
+            i [class "fas fa-book", attribute "aria-hidden" "true"] []
+            , text "parcel"
+          ]
+        ]
+      , a [class "panel-block"] [
+        span [class "panel-icon"] [
+            i [class "fas fa-book", attribute "aria-hidden" "true"] []
+            , text "yarn"
+          ]
+        ]
+      ]
+    ]
+
 
 profilePage: Html msg
 profilePage =
-  p [] [ text "我が名はきゅあれもねーど！世のダークコーディングを倒すよ！" ]
+  div []
+    [ nav [class "panel"]
+      [ p [class "panel-heading"] [ text "Profile"]
+      , p [class "panel-block"] [ text "きゅあれもねーど" ]
+      , p [class "panel-block"] [ text "世のダークコーディングを倒すよ！" ]
+      ]
+    , nav [class "panel"]
+      [ p [class "panel-heading"] [ text "SNS"]
+      , a [class "panel-block", href "https://twitter.com/curelemonade2", target "_blank" ] [text "Twitter"]
+      , a [class "panel-block", href "https://github.com/YuMuuu", target "_blank" ] [text "Github"]
+      , a [class "panel-block", href "https://curelemonade2.hatenablog.com/", target "_blank" ] [text "はてなブログ"]
+      , a [class "panel-block", href "https://scrapbox.io/curelemonade15-07937665/", target "_blank" ] [text "ScrapBox"]
+      ]
+    , nav [class "panel"]
+      [ p [class "panel-heading"] [ text "欲しい物リスト"]
+      , a [class "panel-block" , href "https://www.amazon.jp/hz/wishlist/ls/28TVSSAVIAWDM?ref_=wl_share", target "_blank" ] [text "技術書"]
+      , a [class "panel-block" , href "https://www.amazon.jp/hz/wishlist/ls/21D4MW86YISQO?ref_=wl_share", target "_blank" ] [text "技術書以外"]
+      ]
+  ]
 
 notFountPage: Html msg
 notFountPage = p [] [ text "not found" ]
